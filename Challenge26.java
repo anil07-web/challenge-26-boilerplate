1777d2a192fa22a4f19d7ac63ada9a09c590bb16
anilkumar75001900 <anilkumar75001900@gmail.com>
2021-01-06 21:16:28 +0530

    initial commit

--- Challenge26.java
+++ Challenge26.java
@@ -1,87 +1,132 @@
 import java.util.Scanner;
-public class Challenge26{
-    public static void main(String[] args) {
-        customer_details obj = new customer_details();
-        obj.enter_Details();
-        obj.show_Details();    
-    }
+class Customer{
+   private String firstname;
+   private String lastname;
+   private int age;
+   private String aadhaar;
+   private String address;
+   private long phno;
+   private  static int id=0;
+   private int customerid;
+   private String village;
+   Scanner sc = new Scanner(System.in);
+   public Customer(String fname,String lname,int age,String aadhaar,String address,long phno,String vill ){
+     id++;
+     this.customerid=id;
+     this.firstname=fname;
+     this.lastname=lname;
+     this.age=age;
+     this.aadhaar=aadhaar;
+     this.address=address;
+     this.phno=phno;
+     this.village=vill;
+   }
+   public String getfname(){
+     return firstname;
+   }
+   public String getlname(){
+    return lastname;
+   }
+   public int getage(){
+    return age;
+   }
+   public String getaadhar(){
+    return aadhaar;
+   }
+   public String getaddress(){
+    return address;
+   }
+   public long getphno(){
+    return phno;
+   }
+   public int getid(){
+    return customerid;
+   }
+   public String getvillage(){
+    return village;
+  }
+  public static long count(long n) {
+  if(n==0)
+  return 0;
+  return 1+count(n/10);
+  }
+ 
+  public void byLastName(String entlast,int id,String fname,String lname,int age,String aadhaar,String address,long phno,String vill ) {
+  if(entlast.equalsIgnoreCase(lname))
+  {
+   System.out.println();   
+   System.out.println("**CUSTOMER DETAILS**");          
+   System.out.println("CUSTOMER ID: "+id);
+   System.out.println("Name: "+fname+" "+lname);
+   System.out.println("Age: "+age);
+   System.out.println("Address: "+address);
+   System.out.println("Phone no.  +91"+phno);
+   System.out.println("Aadhaar id: "+aadhaar);
+   System.out.println("village : "+village);
+  }
+  }
+  public void byVillageName(String myvillage,int id,String fname,String lname,int age,String aadhaar,String address,long phno,String vill ) {
+  if(myvillage.equalsIgnoreCase(vill))
+  {           
+   System.out.println(+id+"                "+fname+" "+lname);
+  }
+  }
 }
-class customer_details{
-    int n;
-    String firstname[] = new String[100];
-    String lastname[] = new String[100];
-    int age[] = new int[100];
-    String aadhaar[] = new String[100];
-    String address[] = new String[100];
-    String phno[] = new String[100];
-    int custid[] = new int[100];
+
+class Challenge26{
+  public static void main(String[] args) {
     Scanner obj = new Scanner(System.in);
     Scanner obj1 = new Scanner(System.in);
-    public void enter_Details() {
-        for (int i = 0; i <100; i++) {
+    char ch;
+    int n=0;
+    Customer[] object = new Customer[100];
+		do{
         System.out.println("Enter the First name of customer");
-        firstname[i]=obj.next();
+        String a=obj.next();
         System.out.println("Enter the Last name of customer");
-        lastname[i]=obj.next();
+        String b=obj.next();
         System.out.println("Enter the Age of customer");
-        age[i]=obj.nextInt();
+        int c=obj.nextInt();
         System.out.println("Enter the Aadhaar id of customer");
-        aadhaar[i]=obj.next();
+        String d=obj.next();
         System.out.println("Enter the Address of customer");
-        address[i]=obj1.nextLine();
-        System.out.println("Enter the Phone number of customer");
-        phno[i]=obj.next();
-        custid[i]=i+1;
-        System.out.println("Do you want to continue (y/n)");
-        char c = obj.next().charAt(0);
-        if(c=='y' || c=='Y')
-        continue;
-        else{
-        n=i;    
-        break;
-        }
-        }
-    }
-    public void show_Details(){
-         System.out.println();
-         System.out.println("Details of customer by last name, Enter the last name");
-         String entlast=obj.next();
-         for(int i=0;i<n+1;i++){
-         if(entlast.equalsIgnoreCase(lastname[i]))
-         {
-          System.out.println();   
-          System.out.println("**CUSTOMER DETAILS**");          
-          System.out.println("CUSTOMER ID: "+custid[i]);
-          System.out.println("Name: "+firstname[i]+" "+lastname[i]);
-          System.out.println("Age: "+age[i]);
-          System.out.println("Address: "+address[i]);
-          System.out.println("Phone no.  +91"+phno[i]);
-          System.out.println("Aadhaar id: "+aadhaar[i]);
-         }
-        }
-        int index[]=new int[100];
+        String e=obj1.nextLine();
         int k=0;
-        System.out.println();
-        System.out.println("********A list of customers belonging to a particular taluk or village********");
-        System.out.println();
-        for(int i=0;i<n+1;i++){
-            for(int l=0;l<k;l++){
-            if(i==index[l]){
-            i++;
-            break;
-            }
-            }
-            System.out.println("**"+address[i]+"**");
-            for (int j = i; j < n+1; j++){
-            if(address[i].equalsIgnoreCase(address[j])){   
-             System.out.println("CUSTOMER DETAILS");          
-             System.out.println("CUSTOMER ID      Name");
-             System.out.println(+custid[j]+"                "+firstname[j]+" "+lastname[j]);
-             System.out.println();
-             index[k]=j;
-             k++;
-             }
-            }
+        do{ 
+        System.out.println("Enter the Phone number of customer");
+        long f=obj.nextLong();   
+        if(Customer.count(f)==10){
+        System.out.println("Enter the Village of customer"); 
+        String g=obj1.nextLine();
+        object[n] = new Customer(a,b,c,d,e,f,g);
+        k=0;
         }
-    }
+        else{
+        System.out.println("Please enter 10 digits number only\n");
+        k=1;
+        }
+        }while(k==1);
+        n++;
+        System.out.println("Do you want to Add Customer (y/n)");
+        ch = obj.next().charAt(0);
+        }while( ch == 'y' || ch =='Y');
+        System.out.println("Details of customer by last name, Enter the last name :");
+        String entlast=obj.next();
+        for(int i=0;i<n;i++)
+        object[i].byLastName(entlast,object[i].getid(),object[i].getfname(),object[i].getlname(),object[i].getage(),object[i].getaadhar(),object[i].getaddress(),object[i].getphno(),object[i].getvillage());
+        System.out.println();
+        System.out.println("******A list of customers belonging to a particular taluk or village******");
+        System.out.println("Enter Village Name :");
+        String myvillage = obj.next();
+        System.out.println("**CUSTOMER LIST**");
+        System.out.println();
+        System.out.println("CUSTOMER ID      Name");
+        for(int i=0;i<n;i++)
+        object[i].byVillageName(myvillage,object[i].getid(),object[i].getfname(),object[i].getlname(),object[i].getage(),object[i].getaadhar(),object[i].getaddress(),object[i].getphno(),object[i].getvillage());
+  }
 }
+
+
+
+
+
